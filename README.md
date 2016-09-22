6f10ec1bc3d03331b3ec0974a46cda06068a8ade
Meng72 <matthewkeng@hotmail.com>
2016-09-21 22:28:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Project Title: The Ramones#
+  *I have created this object and its accompanying function as part of an assignment.
+  *The Ramones were chosen as the subject of the object as I find them to be the greatest rock band to ever exist.
+
+#Output#
+  *This application will begin educating the user by displaying the following information in the console: "The greatest band in the world, The Ramones, called New York city their home. They formed in 1974..."
---
+++ Ramones.js
@@ -0,0 +1,16 @@
+var ramones = {
+  bandName: "The Ramones",
+  hometown: "New York",
+  yearFounded: "1974",
+  genre: "punk rock",
+  leadSinger: "Joey Ramone",
+  bassist1: "Dee Dee Ramone",
+  guitarist: "Johnny Ramone",
+  drummer1: "Tommy Ramone",
+  drummer2: "Marky Ramone",
+  bassist2: "C.J. Ramone",
+  chant: "Hey! Ho! Let's Go!"
+
+};
+
+console.log("The greatest band in the world, " + ramones.bandName + ", called " + ramones.hometown + " city their home. They formed in " + ramones.yearFounded + ", and were one of the first " + ramones.genre+ " bands. The group's original line-up consisted of " + ramones.leadSinger + " on vocals, " + ramones.guitarist + " on guitar, " + ramones.bassist1 + " on bass, and " + ramones.drummer1 + " keeping time on the drums. " + ramones.drummer2 + " soon replaced Tommy. Later " + ramones.bassist2 + " would join after Dee Dee left the band. The band would often chant during live shows, in unison with their fans, '" + ramones.chant + "'.");
